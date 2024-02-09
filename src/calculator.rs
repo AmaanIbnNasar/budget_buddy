91cefad43f28a762d25ae5ad4a5a226ecd61944a
Amaan Ibn-Nasar <amaan5260@gmail.com>
2024-02-09 21:12:44 +0000

    The rest

--- src/calculator.rs
+++ src/calculator.rs
@@ -1,23 +1,39 @@
 use chrono::prelude::*;
 use chrono::Utc;
+use serde::Deserialize;
+use serde::Serialize;
 
+use crate::BalanceResponse;
+use crate::PostRequest;
+
+#[derive(PartialEq, Deserialize, Serialize, Debug, Clone)]
 pub struct MonthlySpend {
     name: String,
     date: DateTime<Utc>,
     amount: f32,
 }
 
-pub fn calculate_balance(
-    current_date: DateTime<Utc>,
-    current_balance: f32,
-    spend: f32,
-    monthly_spends: Box<[MonthlySpend]>,
-) -> f32 {
-    monthly_spends
+pub fn calculate_new_balance(post_request: PostRequest) -> BalanceResponse {
+    let PostRequest {
+        spend,
+        current_balance,
+        current_date,
+        monthly_spends,
+    } = post_request;
+    let mut spends_applied: Vec<String> = vec![];
+
+    let new_balance = monthly_spends
         .iter()
         .filter(|spend| spend.date <= current_date)
-        .map(|bill| bill.amount)
-        .fold(current_balance - spend, |acc, x| acc - x)
+        .map(|bill| {
+            spends_applied.push(bill.name.to_string());
+            bill.amount
+        })
+        .fold(current_balance - spend, |acc, x| acc - x);
+    BalanceResponse {
+        new_balance,
+        spends_applied,
+    }
 }
 
 #[cfg(test)]
@@ -26,17 +42,28 @@ mod tests {
     #[test]
     fn test_calculate_balance() {
         let balance = 100.0;
-        let expected_balance = 90.0;
+        let expected_balance = BalanceResponse {
+            new_balance: 90.0,
+            spends_applied: vec![],
+        };
 
         let fixed_time = Utc.with_ymd_and_hms(2024, 3, 1, 0, 0, 0).unwrap();
-        let actual_balance = calculate_balance(fixed_time, balance, 10.0, Box::new([]));
-        println!("{}", actual_balance);
+        let post_request = PostRequest {
+            current_balance: balance,
+            current_date: fixed_time,
+            spend: 10.0,
+            monthly_spends: vec![],
+        };
+        let actual_balance = calculate_new_balance(post_request);
         assert_eq!(actual_balance, expected_balance);
     }
     #[test]
     fn test_calculate_balance_with_monthly_spends() {
         let balance = 100.0;
-        let expected_balance = 80.0;
+        let expected_balance = BalanceResponse {
+            new_balance: 80.0,
+            spends_applied: vec!["Phone Bill".to_string()],
+        };
         let fixed_time = Utc.with_ymd_and_hms(2024, 3, 15, 0, 0, 0).unwrap();
         let phone_bill = MonthlySpend {
             name: String::from("Phone Bill"),
@@ -49,8 +76,14 @@ mod tests {
             amount: 10.0,
         };
         let bills = Box::new([phone_bill, rent_bill]);
+        let post_request = PostRequest {
+            current_balance: balance,
+            current_date: fixed_time,
+            spend: 10.0,
+            monthly_spends: bills.to_vec(),
+        };
 
-        let actual_balance = calculate_balance(fixed_time, balance, 10.0, bills);
+        let actual_balance = calculate_new_balance(post_request);
         assert_eq!(actual_balance, expected_balance);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,83 @@
 pub mod calculator;
-use calculator::calculate_balance;
+use calculator::{calculate_new_balance, MonthlySpend};
 use chrono::{DateTime, Utc};
-use serde::Deserialize;
-use serde_json::{json, Value};
+use lambda_http::{http::StatusCode, run, Error, IntoResponse, Request, Response};
+use lambda_runtime::service_fn;
+use serde::{Deserialize, Serialize};
 
-#[derive(PartialEq, Debug, Deserialize)]
-pub struct RequestBody {
+#[derive(PartialEq, Debug, Deserialize, Serialize)]
+pub struct PostRequest {
     current_balance: f32,
     current_date: DateTime<Utc>,
     spend: f32,
+    monthly_spends: Vec<MonthlySpend>,
 }
 
-fn parse_body(event: Value) -> Result<RequestBody, String> {
-    let body = match event.get("body") {
-        Some(body) => serde_json::from_value(body.clone()).unwrap(),
-        None => Err("No body found")?,
+#[derive(PartialEq, Debug, Deserialize, Serialize)]
+pub struct BalanceResponse {
+    new_balance: f32,
+    spends_applied: Vec<String>,
+}
+
+fn parse_body(event: Request) -> Result<PostRequest, String> {
+    match event.body() {
+        lambda_http::Body::Empty => Err("No body")?,
+        lambda_http::Body::Text(body) => serde_json::from_str(body).map_err(|err| err.to_string()),
+        lambda_http::Body::Binary(_) => Err("Weird body")?,
+    }
+}
+
+async fn lambda_handler(event: Request) -> Result<impl IntoResponse, Error> {
+    let parsed_body = match parse_body(event).and_then(|request| Ok(calculate_new_balance(request)))
+    {
+        Ok(foo) => serde_json::to_string(&foo).unwrap(),
+        Err(err) => err.to_string(),
     };
-    Ok(body)
+    let resp: Response<String> = Response::builder()
+        .status(StatusCode::OK)
+        .header("content-type", "application/json")
+        .body(parsed_body)?;
+
+    Ok(resp)
 }
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    run(service_fn(lambda_handler)).await?;
+    Ok(())
 }
 #[cfg(test)]
 mod tests {
     use super::*;
     use chrono::TimeZone;
+    use lambda_http::Request;
     use serde_json::json;
 
     #[test]
     fn test_parse_body() {
         let event = json!({
-            "body": {
                 "current_balance": 1000,
                 "current_date": "2024-01-01T0:00:00.000000000Z",
-                "spend": 100
-            }
-        });
-        let expected_response = RequestBody {
+                "spend": 100,
+                "monthly_spends": [
+                ]
+        })
+        .to_string();
+        let new_event = Request::new(lambda_http::Body::Text(event));
+        let expected_response = PostRequest {
             current_balance: 1000.0,
             current_date: Utc.with_ymd_and_hms(2024, 1, 1, 0, 0, 0).unwrap(),
             spend: 100.0,
+            monthly_spends: vec![],
         };
-        assert_eq!(parse_body(event).ok(), Some(expected_response))
+        assert_eq!(parse_body(new_event).unwrap(), expected_response)
     }
 
     #[test]
     fn test_parse_body_fails_with_no_body() {
         assert_eq!(
-            parse_body(json!({})).err(),
-            Some(String::from("No body found"))
+            parse_body(Request::default()).unwrap_err(),
+            String::from("No body")
         )
     }
 }
