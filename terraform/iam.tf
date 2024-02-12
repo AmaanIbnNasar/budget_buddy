0aa424bf1612e49862a264c76ee97945d428d210
Amaan Ibn-Nasar <amaan5260@gmail.com>
2024-02-12 17:09:07 +0000

    Renamed role to match action

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_iam_role" "terraform_apply_role" {
-  name = "budget_buddy_terraform_apply_role"
+  name = "budget_buddy_terraform_deploy_role"
   assume_role_policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
